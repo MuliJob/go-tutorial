cedd18af410d16c456fad0a3a490c944a629910a
MuliJob <munyoki912@gmail.com>
2025-06-30 00:44:34 +0300

    :sparkles: Created end point to post tasks todo

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 type Todo struct {
-	ID        int    `json:"id" bson:"_id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+	ID        primitive.ObjectID    `json:"id,omitempty" bson:"_id,omitempty"`
+	Completed bool                  `json:"completed"`
+	Body      string                `json:"body"`
 }
 
 var collection *mongo.Collection
@@ -51,7 +52,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/api/todos", getTodos)
-	// app.Post("/api/todos", createTodo)
+	app.Post("/api/todos", createTodo)
 	// app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
@@ -85,9 +86,28 @@ func getTodos(c *fiber.Ctx) error {
 }
 
 
-// func createTodo(c *fiber.Ctx) error {
-	
-// }
+func createTodo(c *fiber.Ctx) error {
+	todo := new(Todo)
+
+	if err := c.BodyParser(todo); err != nil {
+		return err
+	}
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Body is required",
+		})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), todo)
+	if err != nil {
+		return err
+	}
+
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(todo)
+}
 
 // func updateTodo(c *fiber.Ctx) error {}
 
