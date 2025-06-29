92741e8ad684920cc5bc5507713010e516b3d0dd
MuliJob <munyoki912@gmail.com>
2025-06-30 01:02:06 +0300

    :sparkles: Created an update todo endpoint

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
-	// app.Patch("/api/todos/:id", updateTodo)
+	app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
@@ -109,6 +109,28 @@ func createTodo(c *fiber.Ctx) error {
 	return c.Status(201).JSON(todo)
 }
 
-// func updateTodo(c *fiber.Ctx) error {}
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid ID format",
+		})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set":bson.M{"completed": true}}
+
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"success": "Todo updated successfully",
+	})
+}
 
 // func deleteTodo(c *fiber.Ctx) error {}
